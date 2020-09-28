98982ad8844a945a628e998efb1a9a14bd90c537
Asad Ali <asadalibest1@gmail.com>
2020-09-28 12:48:46 +0500

    Add files via upload
    Rejected project by a company created, in the first module of the course :smile:

---
+++ Bathbed-Collecton/index.js
@@ -0,0 +1,32 @@
+
+function change1() {
+    document.getElementById('DS-h1').innerText = document.getElementById('head_1').innerText;
+    document.getElementById('DS-h3').innerText = document.getElementById('st_1').innerText;
+    document.getElementById('DS-p').innerText = document.getElementById('para_1').innerText;
+
+};
+
+function change2() {
+    document.getElementById('DS-h1').innerText = document.getElementById('head_2').innerText;
+    document.getElementById('DS-h3').innerText = document.getElementById('st_2').innerText;
+    document.getElementById('DS-p').innerText = document.getElementById('para_2').innerText;
+
+};
+function change3() {
+    document.getElementById('DS-h1').innerText = document.getElementById('head_3').innerText;
+    document.getElementById('DS-h3').innerText = document.getElementById('st_3').innerText;
+    document.getElementById('DS-p').innerText = document.getElementById('para_3').innerText;
+
+};
+function change4() {
+    document.getElementById('DS-h1').innerText = document.getElementById('head_4').innerText;
+    document.getElementById('DS-h3').innerText = document.getElementById('st_4').innerText;
+    document.getElementById('DS-p').innerText = document.getElementById('para_4').innerText;
+
+};
+function change5() {
+    document.getElementById('DS-h1').innerText = document.getElementById('head_5').innerText;
+    document.getElementById('DS-h3').innerText = document.getElementById('st_5').innerText;
+    document.getElementById('DS-p').innerText = document.getElementById('para_5').innerText;
+
+};
